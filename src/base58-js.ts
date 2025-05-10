07aa95e0e1ffdb8e59b8d1f794ec1909cdd351a3
Brandon Lehmann <brandonlehmann@gmail.com>
2025-05-10 12:48:30 -0400

    Update dependencies

--- src/base58-js.ts
+++ src/base58-js.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2021, pur3missh
-// Copyright (c) 2023, Brandon Lehmann <brandonlehmann@gmail.com>
+// Copyright (c) 2023-2025, Brandon Lehmann <brandonlehmann@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
